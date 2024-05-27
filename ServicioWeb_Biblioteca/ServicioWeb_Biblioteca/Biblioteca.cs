ea21eca522a8111bf8afdbfc05d05ba59089e386
The12angel21 <132692195+The12angel21@users.noreply.github.com>
2024-05-27 18:44:48 +0200

    Add files via upload

---
+++ ServicioWeb_Biblioteca/ServicioWeb_Biblioteca/Biblioteca.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServicioWeb_Biblioteca
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.Single)]
+    public class Biblioteca : IBiblioteca
+    {
+        int numsaludos = 0;
+
+        public string Saluda(string nombre)
+        {
+            lock (this) { numsaludos++; }
+            string saludo = "Hola, " + nombre + "!";
+            Console.WriteLine("Saludos #{0}: " + saludo, numsaludos);
+            return saludo;
+        }
+
+        public Persona Cumpleaños(Persona persona)
+        {
+            persona.Edad++;
+            return persona;
+        }
+
+        public Persona Yo()
+        {
+            Persona X = new Persona();
+            X.Edad = 46;
+            X.Nombre = "JMMR";
+            return X;
+        }
+
+        public float PesoX(Persona persona, float v)
+        {
+            return persona.Edad * v / 115.5f;
+        }
+    }
+}
---
+++ ServicioWeb_Biblioteca/ServicioWeb_Biblioteca/IBiblioteca.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServicioWeb_Biblioteca
+{
+
+    [ServiceContract]
+    public interface IBiblioteca
+    {
+        [OperationContract]
+        string Saluda(string nombre);
+
+        [OperationContract]
+        Persona Cumpleaños(Persona persona);
+
+        [OperationContract]
+        Persona Yo();
+
+
+        [OperationContract]
+        float PesoX(Persona persona, float v);
+    }
+
+
+    [DataContract]
+    public class Persona
+    {
+        int edad = 20;
+        string nombre = "Hello ";
+
+        [DataMember]
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+        [DataMember]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+    }
+}
